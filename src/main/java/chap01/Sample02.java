579a6ef15373b2bef91e5c6cdc67f92ac9b9a22a
choiinho <ino0319@gmail.com>
2020-07-08 00:29:57 +0900

    sample2

--- src/main/java/chap01/Sample02.java
+++ src/main/java/chap01/Sample02.java
@@ -10,11 +10,14 @@ public class Sample02 {
 		int sum = 0;
 
 		for (int i = 0; i < 10; i++) {
-			System.out.println(i);
-			for (int j = 0; j < 10; j++) {
-				System.out.println("*");
-			}
 
+			for (int j = 0; j < 10; j++) {
+				System.out.print(i);
+				System.out.println(j);
+				sum +=i;
+				System.out.println(sum);
+						
+			}
 		}
 	}
 }
