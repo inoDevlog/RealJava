bd3e5e8cdc828677fd28f4bd07f473048478e2b1
choiinho <ino0319@gmail.com>
2020-07-08 00:20:29 +0900

    sum

--- src/main/java/chap01/Sample01.java
+++ src/main/java/chap01/Sample01.java
@@ -1,6 +1,7 @@
 package chap01;
 
 public class Sample01 {
+	
 	public static void main(String[] args) {
 
 		System.out.print("HHHHHHello, ");
--- src/main/java/chap01/Sample02.java
+++ src/main/java/chap01/Sample02.java
@@ -1,8 +1,20 @@
 package chap01;
 
 public class Sample02 {
+
 	public static void main(String[] args) {
+
 		System.out.println("HHHHHHello, ");
 		System.out.println("       World");
+
+		int sum = 0;
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+			for (int j = 0; j < 10; j++) {
+				System.out.println("*");
+			}
+
+		}
 	}
 }
