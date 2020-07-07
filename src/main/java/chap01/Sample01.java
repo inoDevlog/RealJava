319da4942bd17c0af5e5a9cd03533a427a0c0f6e
choiinho <ino0319@gmail.com>
2020-07-07 23:55:24 +0900

    sample

--- src/main/java/chap01/Sample01.java
+++ src/main/java/chap01/Sample01.java
@@ -2,16 +2,15 @@ package chap01;
 
 public class Sample01 {
 	public static void main(String[] args) {
-		
+
 		System.out.print("HHHHHHello, ");
 		System.out.print("       World");
-		
+
 		int sum = 0;
-		
-		for (int i=0; i<5 ; i++) {
+
+		for (int i = 0; i < 5; i++) {
 			sum = sum + i;
 			System.out.println(sum);
 		}
-		System.out.println(sum);
 	}
 }
