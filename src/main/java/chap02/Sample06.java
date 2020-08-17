691f57386129df144d08ff1e231f5ae9926cecb0
choiinho <ino0319@gmail.com>
2020-08-17 23:52:37 +0900

    casting

--- src/main/java/chap02/Sample06.java
+++ src/main/java/chap02/Sample06.java
@@ -12,11 +12,15 @@ public class Sample06 {
 		int c = 32768;
 		b = (short) c;
 		System.out.println(b);
-		
+
 		int d = -32769;
 		b = (short) d;
+		
+		System.out.println(a);
 		System.out.println(b);
+		System.out.println(c);
 		System.out.println(d);
+
 	}
 
 }
