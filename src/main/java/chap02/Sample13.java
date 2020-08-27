f860fca2bf7c67886acdf111337f5992c7549b97
choiinho <ino0319@gmail.com>
2020-08-27 23:47:36 +0900

    sample

---
+++ src/main/java/chap02/Sample13.java
@@ -0,0 +1,16 @@
+package chap02;
+
+public class Sample13 {
+
+	public static void main(String[] args) {
+
+		String name = "나어때";
+		int age = 28;
+		float height = 159.9f;
+
+		System.out.println("이름 : " + name);
+		System.out.println("나이 : " + age);
+		System.out.println("키 : " + height);
+	}
+
+}
