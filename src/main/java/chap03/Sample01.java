32671e95af1a9afbfb012ea1c8c528fbe5163caf
choiinho <ino0319@gmail.com>
2020-09-14 23:52:43 +0900

    s

--- src/main/java/chap03/Sample01.java
+++ src/main/java/chap03/Sample01.java
@@ -1,5 +1,18 @@
 package chap03;
 
+/**
+ * @author ino
+ *
+ */
 public class Sample01 {
+	public static void main(String[] args) {
+		int apples = 10;
+
+		int people = 2 + 1;
+
+		int remainder = apples % people;
+
+		System.out.println("나머지 : : " + remainder);
+	}
 
 }
