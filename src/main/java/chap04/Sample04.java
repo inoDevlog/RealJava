8be0cad768310b4a5fe408cca6f6b47c1a61dcb4
choiinho <ino0319@gmail.com>
2020-09-19 23:25:57 +0900

    java 11

--- src/main/java/chap04/Sample04.java
+++ src/main/java/chap04/Sample04.java
@@ -3,9 +3,13 @@ package chap04;
 public class Sample04 {
 
 	public static void main(String[] args) {
+
 		int inc = 1;
-		System.out.println(inc++);
+		byte var10001 = (byte) inc;
+		int inc = inc + 1;
+		System.out.println(var10001);
 		System.out.println(inc);
+
 	}
 
 }
